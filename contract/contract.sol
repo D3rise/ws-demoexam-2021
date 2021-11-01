e84abcbeadbced0e66148bb9c97d8ee9d760bac1
D3rise <derise2000@gmail.com>
2021-11-02 02:05:47 +0300

    Add some comments; Create getTemplatesOfCategory function

--- contract/contract.sol
+++ contract/contract.sol
@@ -45,6 +45,7 @@ contract Ledger {
 
     uint256 numTransfers; // общее количество переводов
 
+    // Проверяет, администратор ли пользователь
     modifier onlyAdmin() {
         require(
             users[msg.sender].admin,
@@ -53,7 +54,7 @@ contract Ledger {
         _;
     }
 
-    // Перевод
+    // Создать перевод
     function transfer(
         address payable to,
         bytes32 codewordHash,
@@ -79,27 +80,35 @@ contract Ledger {
         users[msg.sender].sentTransfers.push(transferID);
     }
 
+    // Подтвердить перевод (получатель пишет кодовое слово и если оно верно - получает деньги)
     function verifyTransfer(uint256 transferID, bytes32 codewordHash)
         public
         returns (bool success)
     {
         Transfer memory thisTransfer = transfers[transferID];
+
+        // Если перевод завершен или предназначен не отправителю запроса, возвращаем ошибку
         require(
-            msg.sender == thisTransfer.to,
+            msg.sender == thisTransfer.to && !thisTransfer.done,
             "You can't verify this transfer!"
         );
 
-        require(thisTransfer.value > 0, "Transfer does not exists!");
+        // Проверка кодового слова
         require(codewordHash == thisTransfer.codewordHash, "Wrong codeword!");
 
+        // Адрес получателя
         address payable recipient = payable(msg.sender);
 
+        // Указываем, что перевод успешно осуществлен и завершен
         transfers[transferID].success = true;
         transfers[transferID].done = true;
-        recipient.transfer(thisTransfer.value);
         success = true;
+
+        // Переводим деньги получателю
+        recipient.transfer(thisTransfer.value);
     }
 
+    // Отменить перевод, если он еще не осуществлен
     function revertTransfer(uint256 transferID) public {
         Transfer memory thisTransfer = transfers[transferID];
 
@@ -109,19 +118,57 @@ contract Ledger {
         );
         require(!thisTransfer.success, "This transfer is already done!");
 
-        address payable recipient = payable(msg.sender);
-        recipient.transfer(thisTransfer.value);
+        address payable sender = payable(msg.sender);
+        sender.transfer(thisTransfer.value);
+
+        // Указываем, что перевод завершен (все остальные операции недоступны)
         transfers[transferID].done = true;
     }
 
+    // Получить список переводов за последние daysCount дней
+    function getTransfersOnDays(uint256 daysCount)
+        public
+        view
+        returns (Transfer[] memory allTransfersOnPeriod)
+    {
+        // Начиная с какого тамйштампа брать переводы: текущее время - (количество дней * 24 часа * 60 минут * 60 секунд)
+        uint256 fromTimestamp = block.timestamp -
+            (daysCount * 24 * 60 * 60 * 60);
+
+        // Отправленные и полученные переводы
+        uint256[] memory sentTransfers = users[msg.sender].sentTransfers;
+        uint256[] memory recievedTransfers = users[msg.sender]
+            .recievedTransfers;
+
+        uint256[][2] memory allTransfers = [sentTransfers, recievedTransfers];
+
+        uint256 lastIndex = 0;
+
+        // Проходимся по всем переводам, ищем те что нужно
+        for (uint8 i = 0; i < allTransfers.length; i++) {
+            for (uint256 j = 0; j < allTransfers[i].length; j++) {
+                uint256 thisTransferID = allTransfers[i][j];
+                Transfer memory thisTransfer = transfers[thisTransferID];
+
+                // Если перевод был осуществлен после нужного таймштампа, добавляем его в возвращаемый массив
+                if (thisTransfer.timestamp > fromTimestamp) {
+                    allTransfersOnPeriod[lastIndex++] = thisTransfer;
+                }
+            }
+        }
+    }
+
+    // Получить идентификаторы отправленных переводов
     function getSentTransfers() public view returns (uint256[] memory) {
         return users[msg.sender].sentTransfers;
     }
 
+    // Получить идентификаторы полученных переводов
     function getRecievedTransfers() public view returns (uint256[] memory) {
         return users[msg.sender].recievedTransfers;
     }
 
+    // Получить подробную информацию о переводе по его ID
     function getTransfer(uint256 transferID)
         public
         view
@@ -130,39 +177,7 @@ contract Ledger {
         return transfers[transferID];
     }
 
-    function getTransfersOnDays(uint256 daysCount)
-        public
-        view
-        returns (Transfer[] memory allTransfersOnPeriod)
-    {
-        uint256 fromTimestamp = block.timestamp -
-            (daysCount * 24 * 60 * 60 * 60);
-
-        uint256[] memory sentTransfers = users[msg.sender].sentTransfers;
-        uint256[] memory recievedTransfers = users[msg.sender]
-            .recievedTransfers;
-
-        uint256 lastIndex = 0;
-
-        for (uint256 i = 0; i < sentTransfers.length; i++) {
-            uint256 thisTransferID = sentTransfers[i];
-            Transfer memory thisTransfer = transfers[thisTransferID];
-
-            if (thisTransfer.timestamp > fromTimestamp) {
-                allTransfersOnPeriod[lastIndex++] = thisTransfer;
-            }
-        }
-
-        for (uint256 i = 0; i < recievedTransfers.length; i++) {
-            uint256 thisTransferID = recievedTransfers[i];
-            Transfer memory thisTransfer = transfers[thisTransferID];
-
-            if (thisTransfer.timestamp > fromTimestamp) {
-                allTransfersOnPeriod[lastIndex++] = thisTransfer;
-            }
-        }
-    }
-
+    // Добавить категорию
     function addCategory(string memory name)
         public
         onlyAdmin
@@ -174,6 +189,7 @@ contract Ledger {
         categoryNames.push(name);
     }
 
+    // Добавить шаблон
     function addTemplate(
         string memory name,
         uint256 categoryID,
@@ -186,10 +202,12 @@ contract Ledger {
         templates[name] = newTemplate;
     }
 
+    // Получить названия шаблонов
     function getTemplateNames() public view returns (string[] memory) {
         return templateNames;
     }
 
+    // Получить подробную информацию о шаблоне по его имени
     function getTemplate(string memory name)
         public
         view
@@ -198,15 +216,35 @@ contract Ledger {
         return templates[name];
     }
 
+    // Получить имена категорий
     function getCategoryNames() public view returns (string[] memory) {
         return categoryNames;
     }
 
+    // Получить подробную информацию о категории по её имени
     function getCategory(string memory name)
         public
         view
         returns (Category memory)
     {
         return categories[name];
+    }
+
+    // Получить массив шаблонов в категории
+    function getTemplatesOfCategory(uint256 categoryID)
+        public
+        view
+        returns (string[] memory templatesOfCategory)
+    {
+        uint256 lastIndex = 0;
+        for (uint256 i = 0; i < templateNames.length; i++) {
+            Template memory thisTemplate = templates[templateNames[i]];
+
+            if (thisTemplate.categoryID == categoryID) {
+                templatesOfCategory[lastIndex++] = categories[
+                    categoryNames[categoryID]
+                ].name;
+            }
+        }
     }
 }
